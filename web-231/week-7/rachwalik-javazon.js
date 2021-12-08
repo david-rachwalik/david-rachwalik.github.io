32960d2f5311de844b7b9bd5c338152ac7b02133
David Rachwalik <david.rachwalik@outlook.com>
2021-12-08 00:08:08 -0600

    check-in added for Exercise 7.2

---
+++ web-231/week-7/rachwalik-javazon.js
@@ -0,0 +1,223 @@
+/*
+============================================
+; Title:  rachwalik-javazon.js
+; Author: David Rachwalik
+; Date:   2021/12/05
+; Description: Exercise 7.2 for Bellevue University course WEB-231
+;===========================================
+*/
+
+"use strict";
+(function() {
+    // -------- Define Classes --------
+    class Product {
+        constructor(id, description, price, quantity) {
+            this.id = id;
+            this.description = description;
+            this.price = price;
+            this.quantity = quantity;
+        };
+    
+        // Getter
+        get totalValue() {
+            return this._totalValue();
+        };
+    
+        // Method
+        _totalValue() {
+            return this.price * this.quantity;
+        };
+    };
+    
+    class Service {
+        constructor(id, description, hourlyRate, min) {
+            this.id = id;
+            this.description = description;
+            this.hourlyRate = Number(hourlyRate).toFixed(2);
+            this.min = min;
+        };
+    };
+    
+    class Employee {
+        constructor(id, firstName, lastName, salary) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+        };
+    };
+    
+    // Initialize classes
+    let product1 = new Product(9781118008188, "HTML and CSS: Design and Build Websites", 22.26, 10);
+    let product2 = new Product(9781491952023, "JavaScript: The Definitive Guide 7/E", 41.33, 7);
+    let product3 = new Product(9781449331818, "Learning JavaScript Design Patterns 1/E", 25.99, 3);
+    let product4 = new Product(9781617292422, "Express in Action: Writing, building, and testing Node.js applications 1/E", 34.91, 3);
+    let product5 = new Product(9781491954461, "MongoDB The Definitive Guide", 33.70, 8);
+    let service1 = new Service(100, "Website Design", 150, 25);
+    let service2 = new Service(101, "DevOps Consulting", 125, 50);
+    let service3 = new Service(102, "Database Design", 125, 15);
+    let employee1 = new Employee(1007, "Beethoven", "Ludwig", 13.99);
+    let employee2 = new Employee(1008, "Bach", "Johann", 22.30);
+    let employee3 = new Employee(1009, "Mozart", "Wolfgang", 19.50);
+    let employee4 = new Employee(1010, "Brahms", "Johannes", 12);
+    let employee5 = new Employee(1011, "Haydn", "Joseph", 32.50);
+
+    // -------- Populate Table Values (Product) --------
+    document.getElementById("txtProduct1Id").innerHTML = product1.id;
+    document.getElementById("txtProduct1Description").innerHTML = product1.description;
+    document.getElementById("txtProduct1Price").innerHTML = Number(product1.price).toFixed(2);
+    document.getElementById("txtProduct1Quantity").innerHTML = product1.quantity;
+    document.getElementById("txtProduct1TotalValue").innerHTML = Number(product1.totalValue).toFixed(2);
+    document.getElementById("txtProduct2Id").innerHTML = product2.id;
+    document.getElementById("txtProduct2Description").innerHTML = product2.description;
+    document.getElementById("txtProduct2Price").innerHTML = Number(product2.price).toFixed(2);
+    document.getElementById("txtProduct2Quantity").innerHTML = product2.quantity;
+    document.getElementById("txtProduct2TotalValue").innerHTML = Number(product2.totalValue).toFixed(2);
+    document.getElementById("txtProduct3Id").innerHTML = product3.id;
+    document.getElementById("txtProduct3Description").innerHTML = product3.description;
+    document.getElementById("txtProduct3Price").innerHTML = Number(product3.price).toFixed(2);
+    document.getElementById("txtProduct3Quantity").innerHTML = product3.quantity;
+    document.getElementById("txtProduct3TotalValue").innerHTML = Number(product3.totalValue).toFixed(2);
+    document.getElementById("txtProduct4Id").innerHTML = product4.id;
+    document.getElementById("txtProduct4Description").innerHTML = product4.description;
+    document.getElementById("txtProduct4Price").innerHTML = Number(product4.price).toFixed(2);
+    document.getElementById("txtProduct4Quantity").innerHTML = product4.quantity;
+    document.getElementById("txtProduct4TotalValue").innerHTML = Number(product4.totalValue).toFixed(2);
+    document.getElementById("txtProduct5Id").innerHTML = product5.id;
+    document.getElementById("txtProduct5Description").innerHTML = product5.description;
+    document.getElementById("txtProduct5Price").innerHTML = Number(product5.price).toFixed(2);
+    document.getElementById("txtProduct5Quantity").innerHTML = product5.quantity;
+    document.getElementById("txtProduct5TotalValue").innerHTML = Number(product5.totalValue).toFixed(2);
+
+    document.getElementById("txtService1Id").innerHTML = service1.id;
+    document.getElementById("txtService1Description").innerHTML = service1.description;
+    document.getElementById("txtService1HourlyRate").innerHTML = Number(service1.hourlyRate).toFixed(2);
+    document.getElementById("txtService1Min").innerHTML = service1.min;
+    document.getElementById("txtService2Id").innerHTML = service2.id;
+    document.getElementById("txtService2Description").innerHTML = service2.description;
+    document.getElementById("txtService2HourlyRate").innerHTML = Number(service2.hourlyRate).toFixed(2);
+    document.getElementById("txtService2Min").innerHTML = service2.min;
+    document.getElementById("txtService3Id").innerHTML = service3.id;
+    document.getElementById("txtService3Description").innerHTML = service3.description;
+    document.getElementById("txtService3HourlyRate").innerHTML = Number(service3.hourlyRate).toFixed(2);
+    document.getElementById("txtService3Min").innerHTML = service3.min;
+
+    document.getElementById("txtEmployee1Id").innerHTML = employee1.id;
+    document.getElementById("txtEmployee1FirstName").innerHTML = employee1.firstName;
+    document.getElementById("txtEmployee1LastName").innerHTML = employee1.lastName;
+    document.getElementById("txtEmployee1Salary").innerHTML = Number(employee1.salary).toFixed(2);
+    document.getElementById("txtEmployee2Id").innerHTML = employee2.id;
+    document.getElementById("txtEmployee2FirstName").innerHTML = employee2.firstName;
+    document.getElementById("txtEmployee2LastName").innerHTML = employee2.lastName;
+    document.getElementById("txtEmployee2Salary").innerHTML = Number(employee2.salary).toFixed(2);
+    document.getElementById("txtEmployee3Id").innerHTML = employee3.id;
+    document.getElementById("txtEmployee3FirstName").innerHTML = employee3.firstName;
+    document.getElementById("txtEmployee3LastName").innerHTML = employee3.lastName;
+    document.getElementById("txtEmployee3Salary").innerHTML = Number(employee3.salary).toFixed(2);
+    document.getElementById("txtEmployee4Id").innerHTML = employee4.id;
+    document.getElementById("txtEmployee4FirstName").innerHTML = employee4.firstName;
+    document.getElementById("txtEmployee4LastName").innerHTML = employee4.lastName;
+    document.getElementById("txtEmployee4Salary").innerHTML = Number(employee4.salary).toFixed(2);
+    document.getElementById("txtEmployee5Id").innerHTML = employee5.id;
+    document.getElementById("txtEmployee5FirstName").innerHTML = employee5.firstName;
+    document.getElementById("txtEmployee5LastName").innerHTML = employee5.lastName;
+    document.getElementById("txtEmployee5Salary").innerHTML = Number(employee5.salary).toFixed(2);
+    
+    // Default display for tables
+    let products = document.getElementById("products");
+    let services = document.getElementById("services");
+    let employees = document.getElementById("employees");
+
+
+
+
+
+
+    // let book_lord_of_the_rings = {
+    //     isbn: "0395489326",
+    //     title: "The Lord of the Rings",
+    //     pages: 1214
+    // };
+    // let book_illustrated_man = {
+    //     isbn: "9781451678185",
+    //     title: "The Illustrated Man",
+    //     pages: 279
+    // };
+    // let book_martian = {
+    //     isbn: "0553418025",
+    //     title: "The Martian",
+    //     pages: 387
+    // };
+
+    // let author_jrr_tolkien = {
+    //     name: "J. R. R. Tolkien",
+    //     genre: "Fantasy"
+    // };
+    // let author_rumiko_takahashi = {
+    //     name: "Rumiko Takahashi",
+    //     genre: "Manga"
+    // };
+    // let author_hajime_isayama = {
+    //     name: "Hajime Isayama",
+    //     genre: "Manga"
+    // };
+    
+    // // Default display for tables
+    // let books = document.getElementById("favorite-books");
+    // let authors = document.getElementById("favorite-authors");
+    // books.style.display = "none";
+    // authors.style.display = "none";
+
+    // // Populate initial table values (book)
+    // document.getElementById("txtLordOfTheRingsISBN").innerHTML = book_lord_of_the_rings.isbn;
+    // document.getElementById("txtLordOfTheRingsTitle").innerHTML = book_lord_of_the_rings.title;
+    // document.getElementById("txtLordOfTheRingsPages").innerHTML = book_lord_of_the_rings.pages;
+
+    // document.getElementById("txtIllustratedManISBN").innerHTML = book_illustrated_man.isbn;
+    // document.getElementById("txtIllustratedManTitle").innerHTML = book_illustrated_man.title;
+    // document.getElementById("txtIllustratedManPages").innerHTML = book_illustrated_man.pages;
+
+    // document.getElementById("txtMartianISBN").innerHTML = book_martian.isbn;
+    // document.getElementById("txtMartianTitle").innerHTML = book_martian.title;
+    // document.getElementById("txtMartianPages").innerHTML = book_martian.pages;
+
+    // // Populate initial table values (author)
+    // document.getElementById("txtJrrTolkienName").innerHTML = author_jrr_tolkien.name;
+    // document.getElementById("txtJrrTolkienGenre").innerHTML = author_jrr_tolkien.genre;
+
+    // document.getElementById("txtRumikoTakahashiName").innerHTML = author_rumiko_takahashi.name;
+    // document.getElementById("txtRumikoTakahashiGenre").innerHTML = author_rumiko_takahashi.genre;
+
+    // document.getElementById("txtHajimeIsayamaName").innerHTML = author_hajime_isayama.name;
+    // document.getElementById("txtHajimeIsayamaGenre").innerHTML = author_hajime_isayama.genre;
+
+
+    // On click action
+    function toggleCard() {
+        let listing = document.getElementById("listing");
+        let choice = listing.value;
+        // console.log("choice: " + choice);
+
+        if (choice === "products") {
+            products.style.display = "block";
+            services.style.display = "none";
+            employees.style.display = "none";
+        } else if (choice === "services") {
+            products.style.display = "none";
+            services.style.display = "block";
+            employees.style.display = "none";
+        } else if (choice === "employees") {
+            products.style.display = "none";
+            services.style.display = "none";
+            employees.style.display = "block";
+        } else {
+            products.style.display = "none";
+            services.style.display = "none";
+            employees.style.display = "none";
+        }
+    };
+
+    let submitButton = document.getElementById("btnDisplayListing");
+    submitButton.onclick = toggleCard;
+
+})();
