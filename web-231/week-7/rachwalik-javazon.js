03d55bbd7d9016bf7299738430108242aba57464
David Rachwalik <david.rachwalik@outlook.com>
2021-12-12 20:02:30 -0600

    some code cleanup

--- web-231/week-7/rachwalik-javazon.js
+++ web-231/week-7/rachwalik-javazon.js
@@ -128,70 +128,6 @@
     let services = document.getElementById("services");
     let employees = document.getElementById("employees");
 
-
-
-
-
-
-    // let book_lord_of_the_rings = {
-    //     isbn: "0395489326",
-    //     title: "The Lord of the Rings",
-    //     pages: 1214
-    // };
-    // let book_illustrated_man = {
-    //     isbn: "9781451678185",
-    //     title: "The Illustrated Man",
-    //     pages: 279
-    // };
-    // let book_martian = {
-    //     isbn: "0553418025",
-    //     title: "The Martian",
-    //     pages: 387
-    // };
-
-    // let author_jrr_tolkien = {
-    //     name: "J. R. R. Tolkien",
-    //     genre: "Fantasy"
-    // };
-    // let author_rumiko_takahashi = {
-    //     name: "Rumiko Takahashi",
-    //     genre: "Manga"
-    // };
-    // let author_hajime_isayama = {
-    //     name: "Hajime Isayama",
-    //     genre: "Manga"
-    // };
-    
-    // // Default display for tables
-    // let books = document.getElementById("favorite-books");
-    // let authors = document.getElementById("favorite-authors");
-    // books.style.display = "none";
-    // authors.style.display = "none";
-
-    // // Populate initial table values (book)
-    // document.getElementById("txtLordOfTheRingsISBN").innerHTML = book_lord_of_the_rings.isbn;
-    // document.getElementById("txtLordOfTheRingsTitle").innerHTML = book_lord_of_the_rings.title;
-    // document.getElementById("txtLordOfTheRingsPages").innerHTML = book_lord_of_the_rings.pages;
-
-    // document.getElementById("txtIllustratedManISBN").innerHTML = book_illustrated_man.isbn;
-    // document.getElementById("txtIllustratedManTitle").innerHTML = book_illustrated_man.title;
-    // document.getElementById("txtIllustratedManPages").innerHTML = book_illustrated_man.pages;
-
-    // document.getElementById("txtMartianISBN").innerHTML = book_martian.isbn;
-    // document.getElementById("txtMartianTitle").innerHTML = book_martian.title;
-    // document.getElementById("txtMartianPages").innerHTML = book_martian.pages;
-
-    // // Populate initial table values (author)
-    // document.getElementById("txtJrrTolkienName").innerHTML = author_jrr_tolkien.name;
-    // document.getElementById("txtJrrTolkienGenre").innerHTML = author_jrr_tolkien.genre;
-
-    // document.getElementById("txtRumikoTakahashiName").innerHTML = author_rumiko_takahashi.name;
-    // document.getElementById("txtRumikoTakahashiGenre").innerHTML = author_rumiko_takahashi.genre;
-
-    // document.getElementById("txtHajimeIsayamaName").innerHTML = author_hajime_isayama.name;
-    // document.getElementById("txtHajimeIsayamaGenre").innerHTML = author_hajime_isayama.genre;
-
-
     // On click action
     function toggleCard() {
         let listing = document.getElementById("listing");
@@ -217,7 +153,7 @@
         }
     };
 
-    let submitButton = document.getElementById("btnDisplayListing");
+    const submitButton = document.getElementById("btnDisplayListing");
     submitButton.onclick = toggleCard;
 
 })();
