29a4d7bce1369894b1ad16f127df2e787a4a90cd
David Rachwalik <david.rachwalik@outlook.com>
2022-01-09 13:34:31 -0600

    week-1 web-330 files submitted

---
+++ buwebdev/web-330/js/theme.js
@@ -0,0 +1,50 @@
+/*
+    Title: theme.js
+    Author: David Rachwalik
+    Date: 2022/01/09
+    Description: Theme scripts for WEB-330 site
+ */
+
+function setSelectedTheme(theme, iconMode, iconText) {
+    const iconModeEl = document.getElementById("icon-mode")
+    const iconTextEl = document.getElementById("icon-text")
+    // Apply theme values to the display elements
+    document.body.classList.value = theme
+    iconModeEl.classList.value = `fa ${iconMode} pull-right`
+    iconTextEl.innerHTML = iconText
+}
+
+// Theme set to light by default if not in localStorage
+function setDefaultTheme() {
+    const theme = localStorage.getItem("mode") || "light-theme"
+    const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off"
+    const iconText = localStorage.getItem("iconText") || "Light Mode"
+    setSelectedTheme(theme, iconMode, iconText)
+}
+
+// Switch between the website themes
+function toggleMode(event) {
+    // Update the localStorage
+    if (document.body.classList.value === "light-theme") {
+        localStorage.clear()
+        localStorage.setItem("mode", "dark-theme")
+        localStorage.setItem("iconMode", "fa-toggle-on")
+        localStorage.setItem("iconText", "Dark Mode")
+    } else {
+        localStorage.clear()
+        localStorage.setItem("mode", "light-theme")
+        localStorage.setItem("iconMode", "fa-toggle-off")
+        localStorage.setItem("iconText", "Light Mode")
+    }
+    // Update the display elements
+    setSelectedTheme(localStorage.getItem("mode"), localStorage.getItem("iconMode"), localStorage.getItem("iconText"))
+}
+
+/*
+    Page ready actions
+ */
+
+setDefaultTheme()
+
+// Apply event handlers
+document.getElementById("icon-mode").addEventListener("click", toggleMode)
---
+++ buwebdev/web-330/week-1/README.md
@@ -0,0 +1,6 @@
+# WEB 330 Enterprise JavaScript II
+Bellevue University | Web Development [Degree](http://www.bellevue.edu/degrees/bachelor/web-development-bs "Designed by developers for developers.")
+
+## Contributors
+* Richard Krasso
+* David Rachwalik
