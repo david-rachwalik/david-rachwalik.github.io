f77c53a45539d9d8c67f56e9fd7b53ed4677bff2
David Rachwalik <93161615+NekoGoo@users.noreply.github.com>
2022-10-06 17:22:22 -0500

    limited body styling to layout

--- angular/src/app/modules/portfolio/components/portfolio-layout/portfolio-layout.component.ts
+++ angular/src/app/modules/portfolio/components/portfolio-layout/portfolio-layout.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-layout',
@@ -8,8 +13,23 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   // ViewEncapsulation used for lazy loading module styles
   encapsulation: ViewEncapsulation.None,
 })
-export class PortfolioLayoutComponent implements OnInit {
-  constructor() {}
+// export class PortfolioLayoutComponent implements OnInit {
+export class PortfolioLayoutComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {}
 
-  ngOnInit(): void {}
+  // ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor =
+      '#ccffff';
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage =
+      'linear-gradient(to bottom right, #ccffff, white)';
+  }
+
+  ngOnDestroy() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor =
+      'white';
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage =
+      'none';
+  }
 }
