4fc189ec6aea29a4375ce9ec859c129a167f1633
David Rachwalik <93161615+NekoGoo@users.noreply.github.com>
2022-10-06 18:05:43 -0500

    fixed web-430 pages

--- angular/src/app/modules/portfolio/pages/devops/devops.component.ts
+++ angular/src/app/modules/portfolio/pages/devops/devops.component.ts
@@ -20,21 +20,22 @@ export class DevopsComponent implements OnInit {
       {
         flex: '33.3%',
         header: 'Presentation 4.2 - Continuous Integration (CI)',
-        path: '/portfolio/buwebdev/web-430/ci',
+        // path: '/portfolio/buwebdev/web-430/ci',
+        path: '/buwebdev/web-430/ci',
         content:
           'A detailed presentation that summarizes what Continuous Integration is and how it is used.',
       },
       {
         flex: '33.3%',
         header: 'Presentation 6.2 - Dangers of Change Approval Processes',
-        path: '/portfolio/buwebdev/web-430/ca-processes',
+        path: '/buwebdev/web-430/ca-processes',
         content:
           'A detailed presentation that summarizes the dangers of having poor change management processes as it relates to DevOps.',
       },
       {
         flex: '33.3%',
         header: 'Presentation 7.2 - Technology vs Business',
-        path: '/portfolio/buwebdev/web-430/it-vs-business',
+        path: '/buwebdev/web-430/it-vs-business',
         content:
           'A detailed presentation summarizing the balance between technology-driven versus business-driven decision making.',
       },
@@ -42,14 +43,14 @@ export class DevopsComponent implements OnInit {
         flex: '33.3%',
         header:
           'Presentation 8.2 - Security Controls in Shared Source Code Repositories',
-        path: '/portfolio/buwebdev/web-430/source-control-security',
+        path: '/buwebdev/web-430/source-control-security',
         content:
           'Presentation that summarizes how to have secure controls for a shared source code repository.',
       },
       {
         flex: '33.3%',
         header: 'Presentation 9.2 - Change Management',
-        path: '/portfolio/buwebdev/web-430/change-management',
+        path: '/buwebdev/web-430/change-management',
         content:
           'Presentation that summarizes the details of a software change management process.',
       },
