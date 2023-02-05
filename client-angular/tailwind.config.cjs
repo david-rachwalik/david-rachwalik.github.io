aa9a0bdbca09d8b89b6f553627a93e2e08cff931
David Rachwalik <93161615+NekoGoo@users.noreply.github.com>
2023-02-05 07:42:26 -0600

    matched Tailwind's screen sizes to Angular's

--- client-angular/tailwind.config.cjs
+++ client-angular/tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx,vue,scss}'],
+  // https://tailwindcss.com/docs/adding-custom-styles#customizing-your-theme
   theme: {
     extend: {},
+    // https://tailwindcss.com/docs/screens
+    // https://material.angular.io/cdk/layout/overview#predefined-breakpoints
+    screens: {
+      sm: '600px', // => @media (min-width: 600px) { ... }
+      md: '960px', // => @media (min-width: 960px) { ... }
+      lg: '1280px', // => @media (min-width: 1280px) { ... }
+      xl: '1920px', // => @media (min-width: 1920px) { ... }
+    },
     listStyleType: {
       none: 'none',
       disc: 'disc',
