af9df6f92d4704e229c6c1ae909d787aa8bc679c
David Rachwalik <david.rachwalik@outlook.com>
2023-02-28 06:31:32 -0600

    Tailwind theme configured from Angular Material

--- client-angular/tailwind.config.cjs
+++ client-angular/tailwind.config.cjs
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors');
+
+// https://tailwindcss.com/docs/configuration
+// https://tailwindcss.com/docs/theme
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx,vue,scss}'],
+  // A theme consists of configurations for individual theming systems such as "color" or "typography"
   // https://tailwindcss.com/docs/adding-custom-styles#customizing-your-theme
   theme: {
-    extend: {},
+    // -------- THEME OVERRIDES --------
     // https://tailwindcss.com/docs/screens
     // https://material.angular.io/cdk/layout/overview#predefined-breakpoints
     screens: {
@@ -20,6 +26,25 @@ module.exports = {
       square: 'square',
       roman: 'upper-roman',
     },
+    // -------- THEME EXTENSIONS --------
+    extend: {
+      // copied palette from _theme.scss for Angular Material
+      colors: {
+        primary: colors.indigo,
+        accent: {
+          100: colors.pink[100],
+          200: colors.pink[200],
+          400: colors.pink[400],
+          500: colors.pink[500],
+          600: colors.pink[600],
+        },
+        warn: colors.red,
+      },
+      fontFamily: {
+        sans: ['Oswald', 'Verdana', 'Arial', 'sans-serif'],
+      },
+    },
+    // -------- END OF THEME --------
   },
   plugins: [],
   // add a unique prefix to Tailwind classes to avoid class name overlap
