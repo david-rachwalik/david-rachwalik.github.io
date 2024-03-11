726aa21f1cb8271cc8120ab98f940f682a7d649e
David Rachwalik <david.rachwalik@outlook.com>
2024-03-11 10:39:57 -0500

    phased out flex-layout for css flexbox & grid

--- client-angular/src/app/modules/portfolio/pages/devops/devops.component.ts
+++ client-angular/src/app/modules/portfolio/pages/devops/devops.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 interface ProjectCard {
-  flex: string;
-  cls: string;
+  // flex: string;
+  // cls: string;
   header: string;
   path: string;
   content: string;
@@ -15,13 +15,13 @@ interface ProjectCard {
 })
 export class DevopsComponent implements OnInit {
   projectCards: ProjectCard[];
-  widthOneThird = 'tw-flex-item-1/3';
+  // widthOneThird = 'tw-flex-item-1/3';
 
   constructor() {
     this.projectCards = [
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'Presentation 4.2 - Continuous Integration (CI)',
         // path: '/portfolio/buwebdev/web-430/ci',
         path: '/buwebdev/web-430/ci',
@@ -29,24 +29,24 @@ export class DevopsComponent implements OnInit {
           'A detailed presentation that summarizes what Continuous Integration is and how it is used.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'Presentation 6.2 - Dangers of Change Approval Processes',
         path: '/buwebdev/web-430/ca-processes',
         content:
           'A detailed presentation that summarizes the dangers of having poor change management processes as it relates to DevOps.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'Presentation 7.2 - Technology vs Business',
         path: '/buwebdev/web-430/it-vs-business',
         content:
           'A detailed presentation summarizing the balance between technology-driven versus business-driven decision making.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header:
           'Presentation 8.2 - Security Controls in Shared Source Code Repositories',
         path: '/buwebdev/web-430/source-control-security',
@@ -54,8 +54,8 @@ export class DevopsComponent implements OnInit {
           'Presentation that summarizes how to have secure controls for a shared source code repository.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'Presentation 9.2 - Change Management',
         path: '/buwebdev/web-430/change-management',
         content:
--- client-angular/src/app/modules/portfolio/pages/projects/projects.component.ts
+++ client-angular/src/app/modules/portfolio/pages/projects/projects.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 interface ProjectCard {
-  flex: string;
-  cls: string;
+  // flex: string;
+  // cls: string;
+  style: string;
   header: string;
   path: string;
   content: string;
@@ -15,14 +16,17 @@ interface ProjectCard {
 })
 export class ProjectsComponent implements OnInit {
   projectCards: ProjectCard[];
-  widthFull = 'tw-flex-item-full';
-  widthOneThird = 'tw-flex-item-1/3';
+  // widthFull = 'tw-flex-item-full';
+  // widthOneThird = 'tw-flex-item-1/3';
 
   constructor() {
+    // https://tailwindcss.com/docs/grid-column
+    // https://www.w3schools.com/cssref/pr_grid-column.php
     this.projectCards = [
       {
-        flex: '100%',
-        cls: this.widthFull,
+        style: 'grid-column: span 3;',
+        // flex: '100%',
+        // cls: this.widthFull,
         header: "WEB-450 - Assignment #2 - Bob's Computer Repair Shop *",
         // path: 'https://slytherin-bcrs-web450.herokuapp.com',
         path: 'https://bcrs.azurewebsites.net',
@@ -30,8 +34,9 @@ export class ProjectsComponent implements OnInit {
           'Demo page of Angular <i>BCRS</i>&nbsp; that tracks account role access, repair services, invoices, and data graphs.',
       },
       {
-        flex: '100%',
-        cls: this.widthFull,
+        style: 'grid-column: span 3;',
+        // flex: '100%',
+        // cls: this.widthFull,
         header: 'WEB-450 - Assignment #1 - nodebucket *',
         // path: 'https://rachwalik-nodebucket.herokuapp.com',
         path: 'https://nodebucket.azurewebsites.net',
@@ -39,79 +44,89 @@ export class ProjectsComponent implements OnInit {
           'Demo page of Angular <i>nodebucket</i>&nbsp; that provides a to-do tracker for logged-in users.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        style: 'grid-column: auto;',
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'WEB-231 Index',
         path: '/buwebdev/web-231',
         content:
           'Collection of my WEB-231 projects while at Bellevue University.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        style: 'grid-column: auto;',
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'Card Dealer App',
         path: '/buwebdev/web-231/card-game',
         content:
           'Demo app that shuffles a full deck of cards and presents them to player.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        style: 'grid-column: auto;',
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'Number Guessing Game App',
         path: '/buwebdev/web-231/number-game',
         content:
           'Simple proof of concept app where the player tries to guess a number between 1 and 10.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        style: 'grid-column: auto;',
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'WEB-330 Index',
         path: '/buwebdev/web-330',
         content:
           'Collection of my WEB-330 course projects created during Bellevue University.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        style: 'grid-column: auto;',
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'Future Value Calculator',
         path: '/buwebdev/web-330/future-value',
         content:
           'Simple calculator that determines value based on monthly payment and interest rate.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        style: 'grid-column: auto;',
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'WEB-420 Capstone API *',
         path: 'https://rachwalik-capstone.herokuapp.com',
         content: 'Demo web API for the WEB-420 capstone project.',
       },
       {
-        flex: '100%',
-        cls: this.widthFull,
+        style: 'grid-column: span 3;',
+        // flex: '100%',
+        // cls: this.widthFull,
         header: 'WEB-425 - Assignment 7.3 - Form Validation',
         path: 'https://david-rachwalik.github.io/gpa-calculator-app/session/sign-in',
         content:
           'Demo page of Angular using reactive forms and data validation for <i>GPA Calculator App</i>.',
       },
       {
-        flex: '100%',
-        cls: this.widthFull,
+        style: 'grid-column: span 3;',
+        // flex: '100%',
+        // cls: this.widthFull,
         header: 'WEB-425 - Assignment 8.2 - Server-side Communications',
         path: 'https://david-rachwalik.github.io/in-n-out-books',
         content:
           'Demo page of Angular <i>In-N-Out-Books</i>&nbsp; application using a service with API calls to populate book data.',
       },
       {
-        flex: '100%',
-        cls: this.widthFull,
+        style: 'grid-column: span 3;',
+        // flex: '100%',
+        // cls: this.widthFull,
         header: 'WEB-425 - Assignment 9.2 - Capstone',
         path: 'https://david-rachwalik.github.io/loan-app',
         content:
           'Demo page of Angular <i>Loan App</i>&nbsp; that calculates loan interest and shows communication between components.',
       },
       {
-        flex: '33.3%',
-        cls: this.widthOneThird,
+        style: 'grid-column: auto;',
+        // flex: '33.3%',
+        // cls: this.widthOneThird,
         header: 'WEB-430 Index',
         path: '/buwebdev/web-430',
         content:
