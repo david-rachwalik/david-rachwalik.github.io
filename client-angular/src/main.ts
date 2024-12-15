39b9b8519520415b8b1ff6b0510eaa799a8e5ac5
David Rachwalik <david.rachwalik@outlook.com>
2024-12-15 01:03:31 +0000

    Standalone Migration: Bootstrap the application using standalone APIs

--- client-angular/src/main.ts
+++ client-angular/src/main.ts
@@ -1,7 +1,25 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { AppModule } from './app/app.module';
+import { SharedModule } from '@shared/shared.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+// Only import [BrowserModule, BrowserAnimationsModule] once
+// All other shared/feature modules will import [CommonModule]
+// https://dev.to/sanketmaru/import-once-browser-module-1pie
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      // --- Core imports ---
+      BrowserModule,
+      // --- Routing imports ---
+      AppRoutingModule,
+      // --- Shared imports ---
+      SharedModule,
+    ),
+    provideAnimations(), // BrowserAnimationsModule
+  ],
+}).catch((err) => console.error(err));
