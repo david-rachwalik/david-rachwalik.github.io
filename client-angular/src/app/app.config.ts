758cafd6aea165855dc44ad8609ca70bcf50637e
David Rachwalik <david.rachwalik@outlook.com>
2025-06-13 19:01:02 +0000

    moved app into its own config; added eventCoalescing

---
+++ client-angular/src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
+import { provideMarkdown } from 'ngx-markdown';
+
+import { NGRX_PROVIDERS } from '@shared/ngrx.providers';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    // https://angular.dev/guide/http/interceptors
+    provideHttpClient(withInterceptors([])),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideAnimations(), // BrowserAnimationsModule
+    provideMarkdown(),
+    ...NGRX_PROVIDERS,
+  ],
+};
--- client-angular/src/main.ts
+++ client-angular/src/main.ts
@@ -1,24 +1,8 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-  PreloadAllModules,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
-import { provideMarkdown } from 'ngx-markdown';
 
-import { NGRX_PROVIDERS } from '@shared/ngrx.providers';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    // https://angular.dev/guide/http/interceptors
-    provideHttpClient(withInterceptors([])),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideAnimations(), // BrowserAnimationsModule
-    provideMarkdown(),
-    ...NGRX_PROVIDERS,
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
